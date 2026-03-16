b2bead5bc43c805d87a62c93135c73d367581651
Naveen02052005 <naveenmerugu959@gmail.com>
2026-03-16 16:18:31 +0530

    Fix CloudinaryStorage import

--- cloudconfig.js
+++ cloudconfig.js
@@ -1,23 +1,22 @@
 const cloudinary = require("cloudinary").v2;
-const { CloudinaryStorage } = require("multer-storage-cloudinary")
+const CloudinaryStorage = require("multer-storage-cloudinary").CloudinaryStorage;
 
 cloudinary.config({
     cloud_name: process.env.CLOUD_NAME,
     api_key: process.env.CLOUD_API_KEY,
-    api_secret: process.env.CLOUD_API_SCERET 
+    api_secret: process.env.CLOUD_API_SECRET
 });
 
-
 const storage = new CloudinaryStorage({
-    cloudinary:cloudinary,
-    params:{
-        folder: 'wanderlust_DEV',
-        allowed_formats: ["png","jpg","jpeg"],
+    cloudinary: cloudinary,
+    params: {
+        folder: "wanderlust_DEV",
+        allowed_formats: ["png", "jpg", "jpeg"]
     }
-})
+});
 
 module.exports = {
     cloudinary,
     storage
-}
+};
 // we use this in routes/listing.js
